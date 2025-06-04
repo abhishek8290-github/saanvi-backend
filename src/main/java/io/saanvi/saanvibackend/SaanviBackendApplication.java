c29fc29673862a92dbba246541cc0d03967e8f62
Abhishek <abhishek8290.work@gmail.com>
2025-06-05 01:24:15 +0530

    First commit Setup

---
+++ src/main/java/io/saanvi/saanvibackend/SaanviBackendApplication.java
@@ -0,0 +1,13 @@
+package io.saanvi.saanvibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaanviBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaanviBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/saanvi/saanvibackend/SaanviBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.saanvi.saanvibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaanviBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
